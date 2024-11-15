491170be19acdf5c7864d75af34888001ed77266
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-11-15 11:16:01 +0530

    completing the attribute

--- src/models/server/question.collection.ts
+++ src/models/server/question.collection.ts
@@ -14,4 +14,18 @@ export default async function createQuestionCollection() {
         Permission.delete("users"),
     ])
     console.log("Question collection is created");
+
+    //creating attributes
+    await Promise.all([
+        databases.createStringAttribute(db, questionCollection, "title", 100, true),
+        databases.createStringAttribute(db, questionCollection, "content", 10000, true),
+        databases.createStringAttribute(db, questionCollection, "authorId", 50, true),
+        databases.createStringAttribute(db, questionCollection, "tags", 50, true, undefined, true),
+        databases.createStringAttribute(db, questionCollection, "attachmentId", 50, false),
+    ]);
+
+    console.log("Question attribute created");
+
+    // create Indexes
+    
 }
