a1e0e3bbb18d2aae843d2c30c7395398706b911f
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-02 15:33:57 +0530

    updating the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,28 @@
 import { NextResponse } from 'next/server'
- 
+
 import getOrCreateDB from './models/server/dbSetup'
 import getOrCreateStorage from './models/server/storageSetup'
 
+// This function can be marked `async` if using `await` inside
 export async function middleware() {
-    await Promise.all([
-        getOrCreateDB(),
-        getOrCreateStorage()
-    ])
+  
+  await Promise.all([
+    getOrCreateDB(),
+    getOrCreateStorage()
+  ])
   return NextResponse.next()
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
-    /* match all request paths except for the ones that starts with:
+  /* match all request paths except for the the ones that starts with:
+  - api
+  - _next/static
+  - _next/image
+  - favicon.com
 
-        - api
-        - _next/static
-        - _next/image
-        - favicon.com
-    */
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  */
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
 }
