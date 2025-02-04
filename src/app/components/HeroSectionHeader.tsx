0ba8c5b1528227a8cfa45534467eec788b61eaea
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-04 20:26:02 +0530

    designig the home page

--- src/app/components/HeroSectionHeader.tsx
+++ src/app/components/HeroSectionHeader.tsx
@@ -6,6 +6,7 @@ import ScrollableMarquee from "./ScrollableMarquee";
 import { SparklesText } from "@/components/magicui/sparkles-text";
 import { LineShadowText } from "@/components/magicui/line-shadow-text";
 import Image from "next/image";
+import ScrollableMarqueeVertical from "./ScrollableMarqueeVertical";
 
 const HeroSectionHeader = () => {    
 
@@ -18,17 +19,17 @@ const HeroSectionHeader = () => {
                 color="#ffffff"
                 refresh
             />
-            <div className="relative z-10 grid grid-cols-1 gap-4 md:grid-cols-2 pt-28">
+            <div className="relative z-10 grid grid-cols-1 md:gap-4 md:grid-cols-2 pt-20">
                 <div className="flex items-center justify-center">
-                    <div className="space-y-10 text-center">
-                        <h1 className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-7xl font-bold leading-none tracking-tighter text-transparent">
+                    <div className="space-y-4 md:space-y-10 text-center">
+                        <h1 className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-4xl md:text-7xl font-bold leading-none tracking-tighter text-transparent">
                             HeapFlow
                         </h1>
-                            <SparklesText text="Join a community of developers to ask questions, share insights, and help others solve coding challenges." className="text-center text-xl font-medium leading-7 text-gray-300"/>
-                            <LineShadowText className="text-center text-xl text-gray-400 leading-6 italic" shadowColor="white">
+                            <SparklesText text="Join a community of developers to ask questions, share insights, and help others solve coding challenges." className="text-center text-lg md:text-xl font-medium leading-7 text-gray-300"/>
+                            <LineShadowText className="text-center text-sm md:text-xl text-gray-400 leading-6 italic" shadowColor="white">
                                 “Sharing knowledge is the most fundamental act of friendship. Because it is a way you can give something without losing something.”
                             </LineShadowText>
-                        <p className="text-end text-xl leading-none">
+                        <p className="text-end text-sm md:text-xl leading-none">
                             <strong className="text-gray-300"> — Richard Stallman</strong>
                         </p>
                     </div>
@@ -55,7 +56,8 @@ const HeroSectionHeader = () => {
                 </div>
                 
             </div>
-            <ScrollableMarquee/>
+            <div className="hidden md:flex"><ScrollableMarquee/></div>
+            <div className="flex md:hidden justify-center items-center"><ScrollableMarqueeVertical/></div>
         </div>
     );
 };
--- src/app/components/ScrollableMarqueeVertical.tsx
+++ src/app/components/ScrollableMarqueeVertical.tsx
@@ -2,54 +2,59 @@ import { Marquee } from "@/components/magicui/marquee";
 import { NeonGradientCard } from "@/components/ui/neon-gradient-card";
 
 const features = [
-    {
-      title: "Ask & Answer Questions",
-      description:
-        "Post programming-related questions with rich text formatting. Get accurate answers from the developer community. Use code snippets & images for better clarity."
-    },
-    {
-      title: "Upvote, Downvote & Reputation System",
-      description:
-        "Vote on the best answers. Earn reputation points & badges for contributing. Higher reputation unlocks privileges & trust in the community."
-    },
-    {
-      title: "Search & Discover Solutions",
-      description:
-        "Quickly find relevant questions using tags."
-    },
-    {
-      title: "Tags & Categories",
-      description:
-        "Use tags like JavaScript, Python, React, AI, ML. Filter questions by topics of interest. Discover trending tags & discussions."
-    },
-    {
+  {
+    title: "Ask & Answer Questions",
+    description:
+      "Post programming-related questions with rich text formatting. Use code snippets & images for better clarity.",
+  },
+  {
+    title: "Reputation System",
+    description:
+      "Vote on the best answers. Earn reputation points & badges for contributing. Higher reputation unlocks privileges & trust in the community.",
+  },
+  {
+    title: "Search & Discover Solutions",
+    description:
+      "Find the questions quickly and easily using tags and names. You can also see all the questions in the Question tab",
+  },
+  {
+    title: "Tags & Categories",
+    description:
+      "Use tags like JavaScript, Python, React, AI, ML. Filter questions by topics of interest. Discover trending tags & discussions.",
+  },
+  {
     title: "Comment on Questions",
     description:
-        "Ask for clarifications, provide feedback, or add more context to questions through comments. Keep discussions active and insightful."
-    },
-    {
-      title: "Engage with the Developer Community",
-      description:
-        "Comment, discuss, and collaborate on answers. Interact with developers to expand your knowledge and improve solutions."
-    }
-  ];
+      "Ask for clarifications, provide feedback, or add more context to questions through comments. Keep discussions active and insightful.",
+  },
+  {
+    title: "Engage with Community",
+    description:
+      "Comment, discuss, and collaborate on answers. Interact with developers to expand your knowledge and improve solutions.",
+  },
+];
 
 export default function ScrollableMarqueeVertical() {
   return (
-    <div className="relative flex h-[500px] flex-row items-center justify-center overflow-hidden">
-      <Marquee pauseOnHover vertical className="[--duration:20s]">
+    <div className="relative flex w-[600px] h-[1000px] flex-col items-center justify-center overflow-hidden md:p-20 p-5">
+      <Marquee pauseOnHover vertical className="[--duration:25s]">
         {features.map((feature) => (
-                    <NeonGradientCard key={feature.title} className="max-w-sm items-center justify-center text-center">
-        <div className="flex flex-row items-center gap-2">
-                <div className="flex flex-col">
-                  <figcaption className="text-sm font-medium dark:text-white">
-                    {feature.title}
-                  </figcaption>
-                </div>
+          <NeonGradientCard
+            key={feature.title}
+            className="max-w-sm items-center justify-center text-center"
+          >
+            <div className="flex flex-row items-center gap-2">
+              <div className="flex flex-col">
+                <figcaption className="text-xl font-medium text-[#2dd4cf]">
+                  {feature.title}
+                </figcaption>
               </div>
-              <blockquote className="mt-2 text-sm">{feature.description}</blockquote>
-                  </NeonGradientCard>
-                ))}
+            </div>
+            <blockquote className="mt-3 text-sm text-sky-500">
+              {feature.description}
+            </blockquote>
+          </NeonGradientCard>
+        ))}
       </Marquee>
     </div>
   );
